7fb779b3be378a731b1aede742458cd095152a15
rahmad310317 <rahmadwindisimanullang@gmail.com>
2021-10-02 10:53:25 +0700

    update variabel scss

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import 'assets/scss/style.css';
+import 'assets/scss/style.scss';
 
 
 function App() {
---
+++ src/parts/iconText.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Button  from 'element/Button';
+import 'assets/scss/style.css';
+
+export default function iconText() {
+    return (
+        <Button className="brand-text-icon" href="" type="link">
+            Stay<span className="text-gray-900">cation.</span>
+        </Button>
+    )
+}
